c5d2480b6754d5fbf1754942e2ef1ff101f587d5
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-04-25 13:14:10 +0530

    Using Redux in Root app

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-/**
- * @format
- */
-
+import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
+import { Provider } from 'react-redux';
 
-import App from './App';
-import LoginScreen from './LoginScreen.view';
+import LoginScreen from './Login/LoginScreen.view';
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => LoginScreen);
+import configureStore from './store';
+const store = configureStore();
+
+class LoginScreenApp extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <LoginScreen />
+      </Provider>
+    )
+  }
+}
+
+AppRegistry.registerComponent(appName, () => LoginScreenApp);
