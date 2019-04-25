9af5eea1bcdcc2272ad5b94dfc7c4b1badeb5425
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-04-25 11:47:28 +0530

    Using LoginScreen as the root app

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
+
 import App from './App';
+import LoginScreen from './LoginScreen.view';
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => LoginScreen);
